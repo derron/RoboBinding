906098d148f046a2d937bb96721d76175fe7877b
dhu <dhu@niub.la>
2015-09-01 17:41:42 +0800

    Add BindingLayout annotation
    Change-Id: I6ff08ad3420908a12936dc1a8276d7d1b87f6f3e

---
+++ framework/src/main/java/org/robobinding/annotation/BindingLayout.java
@@ -0,0 +1,19 @@
+package org.robobinding.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by dhu on 15/9/1.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.CLASS)
+public @interface BindingLayout {
+    /**
+     * The set of warnings (identified by the lint issue id) that should be
+     * ignored by lint. It is not an error to specify an unrecognized name.
+     */
+    String[] value();
+}
